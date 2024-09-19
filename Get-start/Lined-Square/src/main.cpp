25c00179290690046b88ae19759177b87d985b2d
dashygo097 <dashygo097@gmail.com>
2024-09-19 15:19:22 +0800

    Add files via upload

---
+++ Get-start/Lined-Square/src/main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <math.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+auto width = 1024,height = 768;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos.x, aPos.y , aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+
+
+GLfloat vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f,
+     -0.5f, 0.5f, 0.0f,
+     -0.5f, -0.5f, 0.0f,
+};
+
+
+int main()
+{
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height ,"Square", NULL, NULL);
+
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create a window object!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    // NOTICE: The position of glfwMakeContextCurrent and gladLoadGL
+    // CANNOT be replaced by each other,or program does not work properly.
+
+    glViewport(0,0,width,height);
+
+        // Vertex Shader
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    
+    glShaderSource(vertexShader, 1 , &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Fragment Shader
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Shader Program
+
+    GLuint shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    // VAO & VBO
+
+    GLuint VAO,VBO;
+    glGenVertexArrays(1 , &VAO);
+    glGenBuffers(1 , &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindVertexArray(VAO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3 ,GL_FLOAT, GL_FALSE, 0, (GLfloat*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+
+    // Program
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.2f,0.3f,0.3f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_LINE_STRIP, 0 , 5);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glDeleteBuffers(1 ,&VBO);
+    glDeleteBuffers(1 ,&VAO);
+    glfwTerminate();
+    return 0;
+
+}
