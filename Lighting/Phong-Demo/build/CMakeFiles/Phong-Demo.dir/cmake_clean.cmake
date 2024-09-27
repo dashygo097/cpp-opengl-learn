b46824b7c9f26565c7f3e3a522c4be5295c212bf
DaShyGo <2684753989@qq.com>
2024-09-27 08:17:18 +0800

    commit again

---
+++ Lighting/Phong-Demo/build/CMakeFiles/Phong-Demo.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Phong-Demo.dir/src/camera.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/camera.cpp.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/ebo.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/ebo.cpp.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/extension.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/extension.cpp.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/glad.c.obj"
+  "CMakeFiles/Phong-Demo.dir/src/glad.c.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/main.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/main.cpp.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/shader.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/shader.cpp.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/stb.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/stb.cpp.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/texture.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/texture.cpp.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/vao.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/vao.cpp.obj.d"
+  "CMakeFiles/Phong-Demo.dir/src/vbo.cpp.obj"
+  "CMakeFiles/Phong-Demo.dir/src/vbo.cpp.obj.d"
+  "G:/OpenGL/Lighting/Phong-Demo/Phong-Demo.exe"
+  "G:/OpenGL/Lighting/Phong-Demo/Phong-Demo.exe.manifest"
+  "G:/OpenGL/Lighting/Phong-Demo/Phong-Demo.pdb"
+  "G:/OpenGL/Lighting/Phong-Demo/libPhong-Demo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Phong-Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
